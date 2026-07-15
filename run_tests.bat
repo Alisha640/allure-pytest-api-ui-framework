4dd581df5c21dddbce9bbad04ba34edd09a94dba
Alisha Khalid <alishakhalid65@gmail.com>
2026-07-16 04:57:36 +0500

    add_allure_pages

--- run_tests.bat
+++ run_tests.bat
@@ -1,6 +1,6 @@
 @echo off
 rem 1. Run the tests and collect results
-py -m pytest --alluredir=reports/allure-results
+rem py -m pytest --alluredir=reports/allure-results 
 
 rem 2. Use 'call' so control returns to this script afterward - converts results to html
 call allure generate reports/allure-results -o reports/allure-report --clean
